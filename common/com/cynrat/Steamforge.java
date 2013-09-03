37ebbb2725bf879f21f56cfcd6fef5d981a1bd56
Daniel Davies <daniel.davies@gmx.co.uk>
2013-09-03 23:19:30 +0100

    Basic outline of mod files needed at inception, .gitignore added

--- common/com/cynrat/Steamforge.java
+++ common/com/cynrat/Steamforge.java
@@ -5,8 +5,11 @@ import com.cynrat.network.PacketHandler;
 import com.cynrat.proxies.CommonProxy;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
@@ -17,11 +20,21 @@ public class Steamforge {
 	@Instance(Reference.ID)
 	public static Steamforge steamforge;
 	
-	@SidedProxy
+	@SidedProxy(clientSide = "com.cynrat.proxies.ClientProxy", serverSide = "com.cynrat.proxies.CommonProxy")
 	public static CommonProxy proxy;
 	
 	@EventHandler
 	public static void init(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void load(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
 		
 	}
 	
